6ebb069206a4de70af73ff62763a99b67f292ea6
Morten <MERL@ITU.DK>
2025-08-29 12:52:43 +0200

    Slop Sloppy

--- Program.cs
+++ Program.cs
@@ -1,8 +1,41 @@
-﻿List<string> cheeps = new() { "Hello, ADBSA students!", "Welcome to the course!", "I hope you had a good summer." };
+﻿using Microsoft.VisualBasic.FileIO;
 
-foreach (var cheep in cheeps)
+List<String[]> tweets = new List<String[]>();
+
+String filepath = "chirp_cli_db.csv";
+TextFieldParser parser = new TextFieldParser(filepath);
+parser.SetDelimiters(",");
+
+while (!parser.EndOfData) {
+    String[] currentTweet = parser.ReadFields();
+    try
+    {
+        if (currentTweet[0] != null && currentTweet[1] != null && currentTweet[2] != null)
+        {
+            tweets.Add(currentTweet);
+        }
+        else
+        {
+            Console.WriteLine("Tweet had null parts");
+        }
+    }
+    catch (IndexOutOfRangeException e)
+    {
+        Console.WriteLine(e.Message + "Tweets parts were not found");
+    }
+}
+
+foreach (String[] tweet in tweets)
 {
-    Console.WriteLine(cheep);
-    Thread.Sleep(1000);
-    // PLEASE PLEASE PLEASE
+    try
+    {
+        int unixTime = int.Parse(tweet[2]);
+        DateTime realTime = DateTimeOffset.FromUnixTimeSeconds(unixTime).UtcDateTime;
+        Console.WriteLine(tweet[0] + " @ " + realTime + ": " + tweet[1]);
+    }
+    catch (FormatException ee)
+    {
+        Console.WriteLine(ee.Message + " Tried to parse invalid string to int");
+        Console.WriteLine("");
+    }
 }
