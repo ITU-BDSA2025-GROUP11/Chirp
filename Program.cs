0d1fcea48a29939e24dc70fe8f36518257605ef0
Milja <miljanoomi@gmail.com>
2025-09-01 20:47:52 +0200

    print cheeps from csv

--- Program.cs
+++ Program.cs
@@ -1,23 +1,62 @@
 ﻿
 using System.IO;
 
-String author;
-String dateTime;
-String message;
-
-try
+class Program
 {
-    // Open the text file using a stream reader.
-    using StreamReader reader = new("/Users/miljajensen/3s/bdsa/Chirp/chirp_cli_db.csv");
+    
+    static void Main()
+    {
+        string author;
+        string dateTime;
+        string message;
+        try
+        {
+            // Open the text file using a stream reader.
+        
+            StreamReader reader = new("/Users/miljajensen/3s/bdsa/Chirp/chirp_cli_db.csv");
+            string line = reader.ReadLine();
+            while (!reader.EndOfStream)
+            {
+                line = reader.ReadLine();
+                // Read the stream as a string.
+                string[] textArray = line.Split('"');
+                author = textArray[0].Replace(",", "");
+                dateTime = textArray[2].Replace(",", "");
+                dateTime = epoch2dateString(dateTime) + " " + epoch2timeString(dateTime);
+                message = textArray[1];
+        
+                string finalString = author + " @ " + dateTime + " " + message;
+        
+                // Write the text to the console.
+                Console.WriteLine(finalString);
+                
+            }
 
-    // Read the stream as a string.
-    string text = reader.ReadToEnd();
+           
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("The file could not be read:");
+            Console.WriteLine(e.Message);
+        }
+    }
 
-    // Write the text to the console.
-    Console.WriteLine(text);
+    static string epoch2dateString(string dateTime) 
+    {
+        int epoch = Int32.Parse(dateTime);
+        return new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(epoch).ToShortDateString(); 
+    }
+    static string epoch2timeString(string dateTime) 
+    {
+        int epoch = Int32.Parse(dateTime);
+        return new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(epoch).ToLongTimeString(); 
+    }
 }
-catch (IOException e)
-{
-    Console.WriteLine("The file could not be read:");
-    Console.WriteLine(e.Message);
-}
+
+
+
+
+
+
+
+
