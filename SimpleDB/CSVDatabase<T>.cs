64006b7732987e06cb65b977365b3239c5bf5031
Emilie Bliddal Ravn Larsen <emilie3103@hotmail.dk>
2025-09-05 13:26:28 +0200

    Relocated folders

---
+++ SimpleDB/CSVDatabase<T>.cs
@@ -0,0 +1,18 @@
+using System.Globalization;
+
+namespace SimpleDB;
+
+sealed class CSVDatabase<T> : IDatabaseRepository<T>
+{
+    public IEnumerable<T> Read(int? limit = null)
+    {
+        using var sw = File.AppendText(path);
+        using var csv = new CsvWriter(sw, CultureInfo.InvariantCulture);
+        csv.WriteRecord(new Cheep(Environment.UserName,input, DateTimeOffset.UtcNow.ToUnixTimeSeconds()));
+        csv.NextRecord();
+    }
+
+    public void Store(T record)
+    {
+    }
+}
