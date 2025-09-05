9a0ea18fa752511d447281128843329e5417d14e
Emilie Bliddal Ravn Larsen <emilie3103@hotmail.dk>
2025-09-05 10:20:27 +0200

    New file configurable file path add
    It should now be possible to write to csv-file no matter what computer you're on

--- Program.cs
+++ Program.cs
@@ -13,7 +13,7 @@ class Program
     private static void Main(String[] args)
     {
         // path needs to be fixed
-        path = @"E:\GitHub\Chirp\chirp_cli_db.csv";
+        path ="chirp_cli_db.csv";
 
         if (args.Length > 0) //Hvis man selv skriver en besked i terminalen
         { 
@@ -90,7 +90,6 @@ class UserInterface()
         {
             return new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(dateTime).ToLongTimeString();
         }
-
-
+        
     }
 }
