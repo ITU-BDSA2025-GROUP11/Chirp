78544cec044de49825530b6ceb3169410e0485ab
Emilie Bliddal Ravn Larsen <emilie3103@hotmail.dk>
2025-09-12 10:28:02 +0200

    Deleted unused fields from program.cs

--- Chirp.CLI/Program.cs
+++ Chirp.CLI/Program.cs
@@ -2,14 +2,6 @@ using Chirp.CSVDB;
 
 class Program
 {
-    private static String path;
-    private static String author;
-    private static String message;
-    private static long epochTime;
-    private static List<Cheep> cheeps;
-    private static CSVDatabase<Cheep> CsvDatabase;
-
-
     private static void Main(String[] args)
     {
         CSVDatabase<Cheep> cheepDB = new CSVDatabase<Cheep>();
