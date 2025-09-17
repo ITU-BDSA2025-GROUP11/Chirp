a3e256732fadfadea37e5a33a40a8bc4410335d1
jpre <jpre@itu.dk>
2025-09-17 10:03:02 +0200

    Unit Tests for CSVDatabase
    Add unit tests for CSVDatabase
    Includes tests for:
    - Read
    - Store
    - Output formatting
    - Epoch time conversion

--- Chirp.CSVDB/CSVDatabase.cs
+++ Chirp.CSVDB/CSVDatabase.cs
@@ -6,17 +6,17 @@ namespace Chirp.CSVDB;
 
 public sealed class CSVDatabase<T> : IDatabaseRepository<T>
 {
-    private static String path;
-    private static List<T> cheeps;
+    private readonly string path;
+    private readonly List<T> cheeps;
 
-    public CSVDatabase()
+    public CSVDatabase(string filePath = "../chirp_cli_db.csv")
     {
         // Read before changing file path !! 
         // chirp_cli_db.csv is one remove from root. '../' cds to the parent folder of root and ->
         // and then looks for chirp_cli_db.csv.
         // changing the path to your local path will break the application for everybody else :(
         
-        path = "../chirp_cli_db.csv";
+        path = filePath;
         cheeps = new List<T>();
     }
 
@@ -73,6 +73,7 @@ public sealed class CSVDatabase<T> : IDatabaseRepository<T>
     /// </returns>
     public IEnumerable<T> Read(int? limit = null)
     {
+        
         try
         {
             StreamReader reader = new(path);
@@ -129,12 +130,12 @@ public sealed class CSVDatabase<T> : IDatabaseRepository<T>
             Console.WriteLine(e.Message);
         }
     }
-        static String Epoch2dateString(long dateTime)
+        public static String Epoch2dateString(long dateTime)
         {
             return new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(dateTime).ToShortDateString();
         }
 
-        static String Epoch2timeString(long dateTime)
+        public static String Epoch2timeString(long dateTime)
         {
             return new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(dateTime).ToLongTimeString();
         }
