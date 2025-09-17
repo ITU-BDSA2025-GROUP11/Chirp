b149177590630558574d495fbc47c44f339ee45b
ThereseSchu <therese.schultz.nielsen@gmail.com>
2025-09-17 16:55:00 +0200

    Made a HttpDatabaseRepo
    co-author: @Joakim-David

---
+++ CsvDbService/Program.cs
@@ -0,0 +1,23 @@
+using Chirp.CSVDB;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+// use your existing CSVDatabase for storage
+var db = new CSVDatabase<Cheep>();
+
+// GET /cheeps
+app.MapGet("/cheeps", () =>
+{
+    var cheeps = db.Read();
+    return Results.Ok(cheeps);
+});
+
+// POST /cheep
+app.MapPost("/cheep", (Cheep cheep) =>
+{
+    db.Store(cheep);
+    return Results.Ok();
+});
+
+app.Run("http://localhost:5000");
