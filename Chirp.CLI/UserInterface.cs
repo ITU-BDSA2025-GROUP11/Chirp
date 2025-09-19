054b0333fd1898275e408f5723bc3b6a953c823f
Milja <miljanoomi@gmail.com>
2025-09-19 10:36:47 +0200

    add print methods to UserInteface.cs from CSVDatabase.cs

--- Chirp.CLI/UserInterface.cs
+++ Chirp.CLI/UserInterface.cs
@@ -1,8 +1,35 @@
+using Chirp.CSVDB;
+
 namespace Chirp.CLI;
 
 public static class UserInterface
 {
-    
+    public static void PrintCheeps(List<Cheep> cheeps)
+    {
+        try
+        {
+            foreach (var cheep in cheeps)
+            {
+                string formattedTime =
+                    (Epoch2dateString(cheep.Timestamp) + " " + Epoch2timeString(cheep.Timestamp) + ":");
+                Console.WriteLine($"{cheep.Author} @ {formattedTime} {cheep.Message}");
+            }
+        }
+        catch (IOException e)
+        {
+            Console.WriteLine("The file could not be read:");
+            Console.WriteLine(e.Message);
+        }
+    }
+    public static String Epoch2dateString(long dateTime)
+    {
+        return new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(dateTime).ToString("dd-MM-yyyy");
+    }
+
+    public static String Epoch2timeString(long dateTime)
+    {
+        return new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(dateTime).ToString("HH:mm:ss");;
+    }
     
     
 }
