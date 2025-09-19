aae57f7f21bada4fd8eeb2f374e70016a69cb7b8
ThereseSchu <therese.schultz.nielsen@gmail.com>
2025-09-19 12:09:45 +0200

    Jeg har fjernet beskyttelse fordi jeg kan!!!!!

--- Chirp.CSVDB.Test/UnitTest1.cs
+++ Chirp.CSVDB.Test/UnitTest1.cs
@@ -2,11 +2,10 @@ namespace Chirp.CSVDB.Test;
 
 public class UnitTest1
 {
-Unit_tests
-=======
+
     //for now, test ONLY if an entry can be received from the database after it was stored in there.
     // the program cannot handle if a chirp has been input in a wrong format
-main
+
     [Fact]
     public void Test1()
     {
