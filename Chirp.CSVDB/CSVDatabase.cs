e7df7196ef2dd313d5cfb3bebd52347ecd1f1ec6
Milja <miljanoomi@gmail.com>
2025-09-19 10:35:10 +0200

    remove print methods

--- Chirp.CSVDB/CSVDatabase.cs
+++ Chirp.CSVDB/CSVDatabase.cs
@@ -120,35 +120,4 @@ public sealed class CSVDatabase<T> : IDatabaseRepository<T>
         csv.WriteRecord(record);
         csv.NextRecord();
     }
-
-
-//Følgende metoder skal refaktorises ifm. opgave 2C:
-    public static void PrintCheeps(List<Cheep> cheeps)
-    {
-        try
-        {
-            foreach (var cheep in cheeps)
-            {
-                //Denne kan måske gøres pænere...
-                string formattedTime =
-                    (Epoch2dateString(cheep.Timestamp) + " " + Epoch2timeString(cheep.Timestamp) + ":");
-                Console.WriteLine($"{cheep.Author} @ {formattedTime} {cheep.Message}");
-            }
-        }
-        catch (IOException e)
-        {
-            Console.WriteLine("The file could not be read:");
-            Console.WriteLine(e.Message);
-        }
-    }
-    public static String Epoch2dateString(long dateTime)
-    {
-        return new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(dateTime).ToString("dd-MM-yyyy");
-    }
-
-    public static String Epoch2timeString(long dateTime)
-    {
-        return new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(dateTime).ToString("HH:mm:ss");;
-    }
-        
 }
