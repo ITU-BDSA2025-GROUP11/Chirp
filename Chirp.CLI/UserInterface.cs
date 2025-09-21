b119a0e549c42b824ee7404019fabdb1d4ff6973
Milja <miljanoomi@gmail.com>
2025-09-21 18:42:00 +0200

    find reason for 403 error the issue was in trying to set the repo url to our web url, this does not work yet and so it should still be localhost

--- Chirp.CLI/UserInterface.cs
+++ Chirp.CLI/UserInterface.cs
@@ -6,6 +6,69 @@ namespace Chirp.CLI;
 public static class UserInterface
 {
    
+    public static void CLI(string[] args,CSVDatabase<Cheep>  cheepDB)
+    {
+        const string usage = @"Chirp CLI.
+
+                Usage:
+                    dotnet.exe chirp <message>
+                    dotnet.exe print
+                    dotnet.exe (-h | --help)
+
+                options: 
+                    -h --help     Show this screen.
+                    chirp <message>  Post a chirp
+                    print    Prints chirps from file
+        ";
+        try
+        {
+            var arguments = new Docopt().Apply(usage, args, version: "Chirp CLI 1.0");
+
+            if (arguments["chirp"].IsTrue)
+            {
+                Console.WriteLine("Chirping to file: \n");
+                var repo = new HttpDatabaseRepository("http://localhost:5000");
+                Chirp(arguments, repo);
+                if (arguments["print"].IsTrue)
+                {
+                    PrintFromRepo(arguments, repo);
+                }
+            }
+            else if (arguments["print"].IsTrue)
+            {
+                PrintFromDb(cheepDB);
+            }
+        }
+        catch (DocoptInputErrorException e)
+        {
+            Console.WriteLine("No CLI args detected\n\nYou have the following CLI options:\n");
+            Console.WriteLine("dotnet run -h or --help        Show this screen.");
+            Console.WriteLine("dotnet run chirp <message>      Post a chirp");
+            Console.WriteLine("dotnet run print     Prints chirps from file\n");
+            Console.WriteLine("Have a good day :)\n");
+        }
+    }
+
+    private static void Chirp(IDictionary<string,ValueObject> arguments, HttpDatabaseRepository repo)
+    {
+        Console.WriteLine("Chirping to service:\n");
+        Cheep cheep = new Cheep(Environment.UserName, arguments["<message>"] + "", DateTimeOffset.UtcNow.ToUnixTimeSeconds());
+        repo.Store(cheep);
+    }
+
+    private static void PrintFromRepo(IDictionary<string, ValueObject> arguments, HttpDatabaseRepository repo)
+    {
+        Console.WriteLine("Printing cheeps from service\n");
+        var cheeps = repo.Read();
+        PrintCheeps(cheeps);
+    }
+
+    private static void PrintFromDb(CSVDatabase<Cheep>  cheepDB)
+    {
+        Console.WriteLine("Printing chirps from file\n");
+        cheepDB.Read();
+        PrintCheeps(cheepDB.Read());
+    }
     public static void PrintCheeps(IEnumerable<Cheep> cheeps)
     {
         try
@@ -32,6 +95,5 @@ public static class UserInterface
     {
         return new DateTime(1970, 1, 1, 0, 0, 0, DateTimeKind.Utc).AddSeconds(dateTime).ToString("HH:mm:ss");;
     }
-    
     
 }
