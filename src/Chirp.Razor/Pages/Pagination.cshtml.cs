689f35aa83b01d88c90c5c2563dcd8b4fce8ab43
Milja <miljanoomi@gmail.com>
2025-10-02 12:56:48 +0200

    framework for pagination of cheeps

--- src/Chirp.Razor/Pages/Pagination.cshtml.cs
+++ src/Chirp.Razor/Pages/Pagination.cshtml.cs
@@ -4,6 +4,13 @@ namespace Chirp.Razor.Pages;
 
 public class PaginationModel : PageModel
 {
+    public int CurrentPage {get; set;}
+    public int PageSize { get; set; } = 32; // cheeps per page
+    
+    public int NumberOfCheeps {get; set;}
+    
+    // how many pages depends on how many cheeps in total + cheeps per page
+    public int PageCount =>  NumberOfCheeps / PageSize;  
     
     
 }
