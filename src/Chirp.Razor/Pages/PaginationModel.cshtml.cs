4178b9a685b0b592aa63b2adc0f482096ab1f1a0
Milja <miljanoomi@gmail.com>
2025-10-04 12:35:32 +0200

    add bindproperty
    this should help with pagination

--- src/Chirp.Razor/Pages/PaginationModel.cshtml.cs
+++ src/Chirp.Razor/Pages/PaginationModel.cshtml.cs
@@ -6,9 +6,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 
 public class PaginationModel : PageModel
 {
+    [BindProperty(SupportsGet = true)]
     public int CurrentPage { get; set; } = 1;
     public int PageSize { get; set; } = 32;
-    //
-    //
-
 }
