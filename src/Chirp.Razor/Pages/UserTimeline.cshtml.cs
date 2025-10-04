607b5e733bdacaaf49ba37c54fd7255efbaab74c
Milja <miljanoomi@gmail.com>
2025-10-04 16:16:49 +0200

    add paging logic to model

--- src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Razor/Pages/UserTimeline.cshtml.cs
@@ -5,18 +5,23 @@ using Models;
 
 namespace Chirp.Razor.Pages;
 
-public class UserTimelineModel : PageModel
+public class UserTimelineModel : PaginationModel
 {
     private readonly ICheepService _service;
     public List<CheepViewModel> Cheeps { get; set; }
-
+    public List<CheepViewModel> CurrentPageCheeps { get; set; }
+    public int NumberOfCheeps => Cheeps.Count;
+    public int TotalPages => (int)Math.Ceiling((double)NumberOfCheeps / PageSize);
     public UserTimelineModel(ICheepService service)
     {
         _service = service;
+        
     }
 
-    public ActionResult OnGet(string author)
+    public ActionResult OnGet(string author, int ? timelinepage)
     {
+        CurrentPage = timelinepage ?? 1;
+        CurrentPageCheeps = _service.GetPaginatedCheepsFromAuthor(author, CurrentPage);
         Cheeps = _service.GetCheepsFromAuthor(author);
         return Page();
     }
