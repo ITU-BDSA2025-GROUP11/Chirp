a659e8116dd1ceddef981ef0535ef8ae0f153f6d
Emilie Bliddal Ravn Larsen <emilie3103@hotmail.dk>
2025-10-07 10:05:03 +0200

    Added second migration because of misspelling of atrtibute name

--- src/datamodel/Author.cs
+++ src/datamodel/Author.cs
@@ -5,7 +5,7 @@ namespace Models;
 public class Author
 {
     [Key]
-    public string userame { get; set; }
+    public string username { get; set; }
     
     public string email { get; set; }
     public ICollection<Cheep> cheeps { get; set; }
