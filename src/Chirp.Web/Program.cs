a98f0e863c82104f4b2d2c07a5927ab8a23aa992
Milja <miljanoomi@gmail.com>
2025-10-15 12:26:58 +0200

    seed + hardcode connection string
    had to hardcode conn string as using it from json kept giving null string thus resulting in the program not knowing anything about the database
    and thus meaning the seeding did not work

--- src/Chirp.Web/Program.cs
+++ src/Chirp.Web/Program.cs
@@ -1,13 +1,12 @@
 using Chirp.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 
-
 var builder = WebApplication.CreateBuilder(args);
 
 
 builder.Services.AddRazorPages();
 
-string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+var connectionString = "Data Source=./Chirp.db";  //builder.Configuration.GetConnectionString("DefaultConnection");
 
 builder.Services.AddDbContext<ChirpDbContext>(options => options.UseSqlite(connectionString));
 builder.Services.AddScoped<ICheepRepository, CheepRepository>();
@@ -25,21 +24,34 @@ app.MapRazorPages();
 
 using (var scope = app.Services.CreateScope())
 {
-    var cheepRepo = scope.ServiceProvider.GetRequiredService<ICheepRepository>();
     
-    // Post a test cheep
-    for (int i = 0; i < 3; i++){
-        cheepRepo.PostCheep("Johnny: You betrayed me! You're not good. You, you're just a chicken. Chip-chip-chip-chip-cheep-cheep.");
-        cheepRepo.PostCheep("Johnny: No, I can't. Anyway, how is your sex life?");
-        cheepRepo.PostCheep("Claudette: I got the results of the test back - I definitely have breast cancer.");
-        cheepRepo.PostCheep("Denny: I just like to watch you guys.");
-        cheepRepo.PostCheep("Mark: What are you talking about? I just saw you!");
-        cheepRepo.PostCheep("Johnny: Don't touch me, motherfucker - geddout.");
+    var db = scope.ServiceProvider.GetRequiredService<ChirpDbContext>();
+    try
+    {
+        Console.WriteLine($"[DEBUG] Database used: {Path.GetFullPath(db.Database.GetDbConnection().DataSource)}");
     }
+    catch (Exception ex)
+    {
+        Console.WriteLine($"[ERROR] {ex}");
+    }
+    
+   var context = scope.ServiceProvider.GetRequiredService<ChirpDbContext>();
+    
+    DbInitializer.SeedDatabase(context);
+    
+    
+    
+    // var cheepRepo = scope.ServiceProvider.GetRequiredService<ICheepRepository>();
+    //
+    // // Post a test cheep
+    // for (int i = 0; i < 3; i++){
+    //     cheepRepo.PostCheep("Johnny: You betrayed me! You're not good. You, you're just a chicken. Chip-chip-chip-chip-cheep-cheep.");
+    //     cheepRepo.PostCheep("Johnny: No, I can't. Anyway, how is your sex life?");
+    //     cheepRepo.PostCheep("Claudette: I got the results of the test back - I definitely have breast cancer.");
+    //     cheepRepo.PostCheep("Denny: I just like to watch you guys.");
+    //     cheepRepo.PostCheep("Mark: What are you talking about? I just saw you!");
+    //     cheepRepo.PostCheep("Johnny: Don't touch me, motherfucker - geddout.");
+    // }
 }
 
 app.Run();
-
-namespace Chirp.Web
-{
-}
