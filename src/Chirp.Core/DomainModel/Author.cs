4c3fd6af23a156b9442b2eb04ea07269cbeb25af
Milja <miljanoomi@gmail.com>
2025-10-15 12:19:22 +0200

    match attributes to initilalizer file

--- src/Chirp.Core/DomainModel/Author.cs
+++ src/Chirp.Core/DomainModel/Author.cs
@@ -2,9 +2,9 @@ namespace Chirp.Core.DomainModel;
 
 public class Author
 {
-    public int Id { get; set; }
+    public int AuthorId { get; set; }
     
-    public required string Username { get; set; }
+    public required string Name { get; set; }
     
     public required string Email { get; set; }
     
