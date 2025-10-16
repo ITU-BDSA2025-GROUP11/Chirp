8d0509a2ce1c98d21cf9396035d6fff3d5d48b4d
Milja <miljanoomi@gmail.com>
2025-10-16 10:56:28 +0200

    add max length to cheeps

--- src/Chirp.Core/DomainModel/Cheep.cs
+++ src/Chirp.Core/DomainModel/Cheep.cs
@@ -1,9 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Chirp.Core.DomainModel;
 
 public class Cheep
 {
     public int CheepId { get; set; }
     
+    [StringLength(160)]
     public string Text { get; set; }
     
     public DateTime TimeStamp { get; set; }
