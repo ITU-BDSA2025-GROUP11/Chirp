1b227da69f18f4e8bf150dd0657a942c527860f9
Therese <therese.schultz.nielsen@gmail.com>
2025-10-16 17:00:44 +0200

    Made test template

---
+++ test/CoreTest/UnitTests.cs
@@ -0,0 +1,27 @@
+using Chirp.Core.DomainModel;
+using Xunit;
+
+namespace clientTest;
+
+public class UnitTests
+{
+    [Fact]
+    public void CanSetAndGetAuthorsName()
+    {
+        
+    }
+    
+    [Fact]
+    public void CanSetAndGetCheepsName()
+    {
+        
+    }
+    
+    [Fact]
+    public void CanCreateDTOs()
+    {
+        
+    }
+    
+    
+}
---
+++ test/InfrastructureTest/UnitTests.cs
@@ -0,0 +1,58 @@
+using Xunit;
+
+namespace databaseTest;
+
+public class UnitTests
+{
+    [Fact]
+    public void CanGetCheeps()
+    {
+        
+    }
+    
+    [Fact]
+    public void CanGetPaginatedCheeps()
+    {
+        
+    }
+    
+    [Fact]
+    public void CanPostCheeps()
+    {
+        
+    }
+    
+    [Fact]
+    public void CanCreateUser()
+    {
+        
+    }
+    
+    [Fact]
+    public void CanSetAndGetAuthorSet()
+    {
+        
+    }
+    
+    [Fact]
+    public void CanSetAndGetCheepsSet()
+    {
+        
+    }
+    
+    [Fact]
+    public void CanCreateDbContext()
+    {
+        
+    }
+    
+    //Can get cheeps
+    //Can get Paginated cheeps
+    //Can post cheeps
+    //Can create user
+        
+    //Can set dbcontext author set
+    //Can set dbcontext cheeps set
+        
+    //Can create dbcontext
+}
