bcc59aaa37da121d122ac44e0817efd0cc3666ab
Therese <therese.schultz.nielsen@gmail.com>
2025-10-18 12:39:09 +0200

    made test censetandgetauthorsname

--- test/CoreTest/UnitTests.cs
+++ test/CoreTest/UnitTests.cs
@@ -1,4 +1,5 @@
 using Chirp.Core.DomainModel;
+using Chirp.Core.DTOs;
 using Xunit;
 
 namespace clientTest;
@@ -8,6 +9,17 @@ public class UnitTests
     [Fact]
     public void CanSetAndGetAuthorsName()
     {
+        var author = new Author
+        {
+            AuthorId = 1,
+            Name = "Jane Doe",
+            Email = "jane@example.com",
+            Cheeps = new List<Cheep>()
+        };
+        
+        Assert.True(author.AuthorId == 1);
+        Assert.Equal("Jane Doe", author.Name);
+        Assert.Equal("jane@example.com", author.Email);
         
     }
     
