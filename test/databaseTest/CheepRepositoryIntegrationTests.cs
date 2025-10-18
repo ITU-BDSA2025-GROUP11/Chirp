d577baeb85415baa6a00294137242c77ef5c678c
jpre <jpre@itu.dk>
2025-10-18 09:12:46 +0200

    Update CheepRepositoryIntegrationTests.cs

--- test/databaseTest/CheepRepositoryIntegrationTests.cs
+++ test/databaseTest/CheepRepositoryIntegrationTests.cs
@@ -1,34 +1,69 @@
 ﻿using Xunit;
 using Chirp.Infrastructure;
 using Microsoft.EntityFrameworkCore;
-using Chirp.Core.DomainModel;
 using Microsoft.Extensions.Logging;
+using Chirp.Core.DomainModel;
+using System.Linq;
 
 namespace DatabaseTest;
 
-public class CheepRepositoryIntegrationTests
+public class CheepRepositoryIntegrationTests : IDisposable
 {
-    //Tester om vi kan vi kan tilføje og hente Cheeps fra et datbase.
-    [Fact]
-    public void AddAndRetrieveCheep()
+    private readonly ChirpDbContext _context;
+    private readonly CheepRepository _repo;
+
+    public CheepRepositoryIntegrationTests()
     {
+        // Setup in-memory SQLite DB
         var options = new DbContextOptionsBuilder<ChirpDbContext>()
             .UseSqlite("Data Source=:memory:")
             .Options;
 
-        using var context = new ChirpDbContext(options);
-        context.Database.OpenConnection();
-        context.Database.EnsureCreated();
+        _context = new ChirpDbContext(options);
+        _context.Database.OpenConnection();
+        _context.Database.EnsureCreated();
 
-        var repo = new CheepRepository(context, new LoggerFactory());
-        
-        repo.CreateUser();
-        repo.PostCheep("Joakim er faktisk pænt handsome OG har rigtig god humor");
+        _repo = new CheepRepository(_context, new LoggerFactory());
+    }
 
-        var cheeps = repo.GetCheeps();
-        
+    [Fact]
+    public void Add_And_Retrieve_Cheep()
+    {
+        _repo.CreateUser();
+        _repo.PostCheep("Joakim er faktisk pænt handsome OG har rigtig god humor");
+
+        var cheeps = _repo.GetCheeps();
+
         Assert.Single(cheeps);
         Assert.Equal("Joakim er faktisk pænt handsome OG har rigtig god humor", cheeps[0].Text);
     }
-}
 
+    [Fact]
+    public void PostCheep_ShouldLinkToCorrectAuthor()
+    {
+        _repo.CreateUser();
+        _repo.PostCheep("Integration test cheep");
+
+        var cheep = _context.Cheeps.Include(c => c.Author).First();
+
+        Assert.Equal(Environment.UserName, cheep.Author.Name);
+        Assert.Equal("Integration test cheep", cheep.Text);
+    }
+
+    [Fact]
+    public void PostCheep_WithoutUser_ShouldNotThrow()
+    {
+        _context.Authors.RemoveRange(_context.Authors);
+        _context.SaveChanges();
+
+        var ex = Record.Exception(() => _repo.PostCheep("This should still work"));
+
+        Assert.Null(ex);
+    }
+    
+    public void Dispose()
+    {
+        _context.Database.CloseConnection();
+        _context.Dispose();
+    }
+}
