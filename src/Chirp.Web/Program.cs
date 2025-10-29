1930b36abbdcb224e8e6c3b1496a5fcefd0f9810
Morten <MERL@ITU.DK>
2025-10-29 15:27:54 +0100

    ?

--- src/Chirp.Web/Program.cs
+++ src/Chirp.Web/Program.cs
@@ -1,27 +1,18 @@
 using Chirp.Infrastructure;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-
 builder.Services.AddRazorPages();
 
-// commented this out to make migration work? tell if i fucked something -merl
-// var connectionString = "Data Source=./Chirp.db";  //builder.Configuration.GetConnectionString("DefaultConnection");
-var dbDir = Path.Combine(AppContext.BaseDirectory, "Database");
-Directory.CreateDirectory(dbDir);
-var dbPath = Path.Combine(dbDir, "Chirp.db");
-var connectionString = $"Data Source={dbPath}";
+// path
+var connectionString = "Data Source=./Chirp.db";  //builder.Configuration.GetConnectionString("DefaultConnection");// path end
 
 builder.Services.AddDbContext<ChirpDbContext>(options => options.UseSqlite(connectionString));
 builder.Services.AddScoped<ICheepRepository, CheepRepository>();
+
 builder.Services.AddDefaultIdentity<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
     .AddEntityFrameworkStores<ChirpDbContext>();
-
-builder.Services.AddScoped<ICheepRepository, CheepRepository>();
-
 
 var app = builder.Build();
 
@@ -40,8 +31,11 @@ app.MapRazorPages();
 
 using (var scope = app.Services.CreateScope())
 {
+
     
     var db = scope.ServiceProvider.GetRequiredService<ChirpDbContext>();
+    // db.Database.Migrate();
+    
     try
     {
         Console.WriteLine($"[DEBUG] Database used: {Path.GetFullPath(db.Database.GetDbConnection().DataSource)}");
@@ -50,24 +44,11 @@ using (var scope = app.Services.CreateScope())
     {
         Console.WriteLine($"[ERROR] {ex}");
     }
-    
-   var context = scope.ServiceProvider.GetRequiredService<ChirpDbContext>();
-    
+
+    var context = scope.ServiceProvider.GetRequiredService<ChirpDbContext>();
+
     DbInitializer.SeedDatabase(context);
     
-    
-    
-    // var cheepRepo = scope.ServiceProvider.GetRequiredService<ICheepRepository>();
-    //
-    // // Post a test cheep
-    // for (int i = 0; i < 3; i++){
-    //     cheepRepo.PostCheep("Johnny: You betrayed me! You're not good. You, you're just a chicken. Chip-chip-chip-chip-cheep-cheep.");
-    //     cheepRepo.PostCheep("Johnny: No, I can't. Anyway, how is your sex life?");
-    //     cheepRepo.PostCheep("Claudette: I got the results of the test back - I definitely have breast cancer.");
-    //     cheepRepo.PostCheep("Denny: I just like to watch you guys.");
-    //     cheepRepo.PostCheep("Mark: What are you talking about? I just saw you!");
-    //     cheepRepo.PostCheep("Johnny: Don't touch me, motherfucker - geddout.");
-    // }
 }
 
 app.Run();
