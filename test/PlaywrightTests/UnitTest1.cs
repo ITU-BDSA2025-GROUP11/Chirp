67f31ed1f2d5b4bf322123e10b2bff7ed9f8c744
Emilie Bliddal Ravn Larsen <emilie3103@hotmail.dk>
2025-11-14 11:07:10 +0100

    insert dummy test

--- test/PlaywrightTests/UnitTest1.cs
+++ test/PlaywrightTests/UnitTest1.cs
@@ -1,24 +1,29 @@
-﻿namespace PlaywrightTests;
-[Parallelizable(ParallelScope.Self)]
-[TestFixture]
+﻿using System.Text.RegularExpressions;
+using Microsoft.Playwright;
+using Microsoft.Playwright.Xunit;
 
-public class Tests : PageTest
+namespace PlaywrightTests;
+
+public class UnitTest1: PageTest
 {
-    [Test]
-    public async Task HomepageHasPlaywrightInTitleAndGetStartedLinkLinkingtoTheIntroPage()
+    [Fact]
+    public async Task HasTitle()
     {
         await Page.GotoAsync("https://playwright.dev");
+
         // Expect a title "to contain" a substring.
         await Expect(Page).ToHaveTitleAsync(new Regex("Playwright"));
+    }
 
-        // create a locator
-        var getStarted = Page.GetByRole(AriaRole.Link, new() { Name = "Get started" });
-        // Expect an attribute "to be strictly equal" to the value.
-        await Expect(getStarted).ToHaveAttributeAsync("href", "/docs/intro");
+    [Fact]
+    public async Task GetStartedLink()
+    {
+        await Page.GotoAsync("https://playwright.dev");
 
         // Click the get started link.
-        await getStarted.ClickAsync();
-        // Expects the URL to contain intro.
-        await Expect(Page).ToHaveURLAsync(new Regex(".*intro"));
-    }
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Get started" }).ClickAsync();
+
+        // Expects page to have a heading with the name of Installation.
+        await Expect(Page.GetByRole(AriaRole.Heading, new() { Name = "Installation" })).ToBeVisibleAsync();
+    } 
 }
