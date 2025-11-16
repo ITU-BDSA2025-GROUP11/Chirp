efef591fe8c13cf1548c24b99f5af0e7a91653b9
jpre <jpre@itu.dk>
2025-11-16 19:46:10 +0100

    Changed the FollowingList
    Changed the FollowingList from being required to just initializing authors with an empty list

--- src/Chirp.Core/DomainModel/Author.cs
+++ src/Chirp.Core/DomainModel/Author.cs
@@ -11,7 +11,8 @@ public class Author : IdentityUser
     public required string Email { get; set; }*/
     public required ICollection<Cheep> Cheeps { get; set; }
     
-    public required ICollection<Author> FollowingList { get; set; }
+    // In Author.cs
+    public ICollection<Author> FollowingList { get; set; } = new List<Author>();
 
    // public string userID { get; set; } = default!;
 }
