fe63ce528aaf9d8b6cd22b69dbb8627cf573ba1c
Milja <miljanoomi@gmail.com>
2025-11-21 17:55:48 +0100

    make username and email required

--- src/Chirp.Core/DTO/AuthorDTO.cs
+++ src/Chirp.Core/DTO/AuthorDTO.cs
@@ -2,8 +2,8 @@
 {
     public class AuthorDTO
     {
-        public string Id { get; set; }
-        public string Username { get; set; } = string.Empty;
-        public string Email { get; set; } = string.Empty;
+        public required string Id { get; set; }
+        public required string Username { get; set; }
+        public required string Email { get; set; } 
     }
 }
