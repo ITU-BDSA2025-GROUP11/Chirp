f5e9e6665527947996eb7640ba0fcd293f268858
Milja <miljanoomi@gmail.com>
2025-11-27 17:07:36 +0100

    change namespace to match convention

--- src/Chirp.Core/DTO/UserInfoDTO.cs
+++ src/Chirp.Core/DTO/UserInfoDTO.cs
@@ -1,9 +1,10 @@
-﻿using Chirp.Core.DTOs;
-
-public class UserInfoDTO
-{
-    public string Name { get; set; } = string.Empty;
-    public string Email { get; set; } = string.Empty;
-    public List<CheepDTO> Cheeps { get; set; } = new List<CheepDTO>();
-    public List<string> FollowedUsernames { get; set; } = new List<string>();
+﻿namespace Chirp.Core.DTO
+{ 
+    public class UserInfoDTO
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
+        public List<CheepDTO> Cheeps { get; set; } = new List<CheepDTO>();
+        public List<string> FollowedUsernames { get; set; } = new List<string>();
+    }
 }
