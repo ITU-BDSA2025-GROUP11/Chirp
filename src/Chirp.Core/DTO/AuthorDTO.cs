e7efa0b3cf82251bb9a91b689ccf7b7f97504eaa
Therese <therese.schultz.nielsen@gmail.com>
2025-11-28 14:30:27 +0100

    Vi skifter driver/navigator nu
    Co-Authored-By: milyuh <206851585+milyuh@users.noreply.github.com>

--- src/Chirp.Core/DTO/AuthorDTO.cs
+++ src/Chirp.Core/DTO/AuthorDTO.cs
@@ -7,9 +7,10 @@ namespace Chirp.Core.DTO
         public string Id { get; set; } = string.Empty;
         public string Username { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
-        
-        public virtual ICollection<Author> Following { get; set; } = new List<Author>();
-        
-        public virtual ICollection<Author> Followers { get; set; } = new List<Author>();
+        public ICollection<Cheep>  Cheeps { get; set; } = new List<Cheep>();
+        public ICollection<Cheep>  LikedCheeps { get; set; } = new List<Cheep>();
+        public ICollection<Cheep>  DislikedCheeps { get; set; } = new List<Cheep>();
+        public ICollection<Author> Following { get; set; } = new List<Author>();
+        public ICollection<Author> Followers { get; set; } = new List<Author>();
     }
 }
