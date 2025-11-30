180174c9d7ac30d5500858b34710a63d56aff8de
Milja <miljanoomi@gmail.com>
2025-11-30 21:03:36 +0100

    add like and dislike logic

--- src/Chirp.Web/Pages/UserTimeline.cshtml.cs
+++ src/Chirp.Web/Pages/UserTimeline.cshtml.cs
@@ -33,20 +33,20 @@ namespace Chirp.Web.Pages
         public async Task<IActionResult> OnGet(string author, int? timelinepage)
         {
             if (_authorService.IsUserDeleted(author).Result) return NotFound();
+            Cheeps =  await _cheepService.GetCheeps(author);
             CurrentPage = timelinepage ?? 1;
             var ownTimeline = User.Identity?.IsAuthenticated == true && User.Identity?.Name == author;
-
             if (ownTimeline)
             {
                 NumberOfCheeps = await _cheepService.GetCheepCountFromAuthorAndFollowing(author); 
-                CurrentPageCheeps = await _cheepService.GetCheepsFromAuthorAndFollowing(CurrentPage, 32, author);
+                CurrentPageCheeps = await _cheepService.GetCheepsFromAuthorAndFollowing(CurrentPage-1, 32, author);
             }
             else
             {
                 var allCheeps = await _cheepService.GetCheeps(author); 
                 NumberOfCheeps = allCheeps.Count;
         
-                CurrentPageCheeps = await _cheepService.GetPaginatedCheeps(CurrentPage - 1, 32, author);
+                CurrentPageCheeps = await _cheepService.GetPaginatedCheeps(CurrentPage-1, 32, author);
             }
 
             if (User.Identity?.IsAuthenticated == true)
@@ -55,6 +55,8 @@ namespace Chirp.Web.Pages
                 if (currentUserId != null)
                 {
                     ViewData["Following"] = await _authorService.GetFollowedIds(currentUserId);
+                    ViewData["LikedCheeps"] = await _authorService.GetLikedCheepIds(currentUserId);
+                    ViewData["DislikedCheeps"] = await _authorService.GetDislikedCheepIds(currentUserId);
                 }
             }
 
@@ -67,14 +69,13 @@ namespace Chirp.Web.Pages
             {
                 return RedirectToPage(new { author });
             }
-
-            var user = await _userManager.GetUserAsync(User);
-            if (user == null)
+            var currentUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (currentUserId == null)
             {
                 return Challenge();
             }
 
-            await _cheepService.PostCheep(Message, user.UserName ?? "unknown", user.Email ?? "unknown"); 
+            await _cheepService.PostCheep(Message, currentUserId); 
 
             return RedirectToPage(new { author });
         }
@@ -99,6 +100,39 @@ namespace Chirp.Web.Pages
             }
             var author = RouteData.Values["author"] as string;
             return RedirectToPage(new { author });
+        }
+        public async Task<IActionResult> OnPostLike(int cheepId)
+        {
+            Console.WriteLine("I AM LIKING CHEEP: " + cheepId);
+            //var cheepid =  
+            var currentUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (currentUserId != null)
+            {
+                await _cheepService.LikePost(currentUserId, cheepId);
+            }
+        
+            return RedirectToPage();
+        }
+        public async Task<IActionResult> OnPostUnlike(int cheepId)
+        {
+            var currentUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (currentUserId != null)
+            {
+                await _cheepService.RemoveLike(currentUserId, cheepId);
+            }
+        
+            return RedirectToPage();
+        }
+        
+        public async Task<IActionResult> OnPostDislike(int cheepId)
+        {
+            var currentUserId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (currentUserId != null)
+            {
+                await _cheepService.DislikePost(currentUserId, cheepId);
+            }
+        
+            return RedirectToPage();
         }
         public int GetTotalPages(int numberOfCheeps, int pageSize)
         {
