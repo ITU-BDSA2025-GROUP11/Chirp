e2d924c83163d89d48c187b2b80a83b5c462298f
Therese <therese.schultz.nielsen@gmail.com>
2025-12-02 10:59:10 +0100

    planning which methods to move

--- src/Chirp.Infrastructure/AuthorService.cs
+++ src/Chirp.Infrastructure/AuthorService.cs
@@ -20,15 +20,50 @@ public class AuthorService : IAuthorService
     private readonly AuthorRepository _authorRepository;
     private readonly ILogger<AuthorService> _logger;
 
+
+    public AuthorService(AuthorRepository authorRepository, ILogger<AuthorService> logger)
+    {
+        _authorRepository = authorRepository;
+        _logger = logger;
+    }
+    
     Task<List<string>> GetFollowedIds(string userId)
     {
         
     }
-    Task FollowUser(string currentUserId, string authorIdToFollow);
-    Task UnfollowUser(string currentUserId, string authorIdToUnfollow);
-    Task<bool> IsFollowing(string currentUserId, string authorId);  
-    Task<bool> DeleteUser(string username);
-    Task<bool> IsUserDeleted(string username);
-    public Task<List<int>> GetLikedCheepIds(string userId);
-    public Task<List<int>> GetDislikedCheepIds(string userId);
+
+    Task FollowUser(string currentUserId, string authorIdToFollow)
+    {
+        
+    }
+
+    Task UnfollowUser(string currentUserId, string authorIdToUnfollow)
+    {
+        
+    }
+
+    Task<bool> IsFollowing(string currentUserId, string authorId)
+    {
+        
+    }
+
+    Task<bool> DeleteUser(string username)
+    {
+        
+    }
+
+    Task<bool> IsUserDeleted(string username)
+    {
+        return await GetUserInfo(username) == null;
+    }
+
+    public Task<List<int>> GetLikedCheepIds(string userId)
+    {
+        
+    }
+
+    public Task<List<int>> GetDislikedCheepIds(string userId)
+    {
+        
+    }
 }
