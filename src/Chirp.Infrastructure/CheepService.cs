1b5fd0e405dff7d3f2191bdefa55153baaa486dc
Therese <therese.schultz.nielsen@gmail.com>
2025-12-02 10:59:10 +0100

    planning which methods to move

--- src/Chirp.Infrastructure/CheepService.cs
+++ src/Chirp.Infrastructure/CheepService.cs
@@ -1,6 +1,22 @@
+using Chirp.Core.DTO;
+using Microsoft.Extensions.Logging;
+
 namespace Chirp.Infrastructure;
 
-public interface CheepService
+public interface ICheepService
 {
-    
+    Task<List<CheepDTO>> GetPaginatedCheeps(int currentPage, int pageSize, string? author = null);  
+    Task<List<CheepDTO>> GetCheepsFromAuthorAndFollowing(int page, int pageSize, string authorName);
+    Task<int> GetCheepCountFromAuthorAndFollowing(string authorName);
+    Task<int> GetCheepCount(string? author = null);
+    public Task LikePost(string currentUserId, int cheepIdToLike);
+    public Task DislikePost(string currentUserId, int cheepIdToDislike);
+    public Task RemoveDislike(string currentUserId, int cheepIdToUndislike);
+    public Task RemoveLike(string currentUserId, int cheepIdToUnLike);
+}
+
+public class CheepService
+{
+    CheepRepository _cheepRepository;
+    ILogger<CheepService> _logger;
 }
